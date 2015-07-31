548d54a565ea065538124bfbd9b77481f3e47ed7
snake <lib0707@gmail.com>
2015-07-31 17:01:17 +0800

    Turning the JSON into SearchResult objects

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -60,6 +60,28 @@ class SearchViewController: UIViewController {
         }
         return nil
     }
+    
+    func parseJSON(jsonString: String) -> [String: AnyObject]? {
+        if let data = jsonString.dataUsingEncoding(NSUTF8StringEncoding) {
+            var error: NSError?
+            if let json = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(0), error: &error) as? [String: AnyObject] {
+                return json
+            } else if let error = error {
+                println("JSON Error: \(error)")
+            } else {
+                println("Unknown JSON Error")
+            }
+        }
+        return nil
+    }
+    
+    func showNetworkError() {
+        let alert = UIAlertController(title: "Whoops...", message: "There was an error reading from the iTunes Store. Please try again.", preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alert.addAction(action)
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
 }
 
 extension SearchViewController: UISearchBarDelegate {
@@ -74,10 +96,16 @@ extension SearchViewController: UISearchBarDelegate {
             println("URL: '\(url)'")
             
             if let jsonString = performStoreRequestWithURL(url) {
-                println("Received JSON string '\(jsonString)'")
+//                println("Received JSON string '\(jsonString)'")
+                if let dictionary = parseJSON(jsonString) {
+                    println("Dictionary \(dictionary)")
+                    
+                    tableView.reloadData()
+                    return
+                }
             }
             
-            tableView.reloadData()
+            showNetworkError()
         }
     }
     
