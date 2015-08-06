05fed851ad415b4cebeb92cbfbb6f4688cf00e2a
snake <lib0707@gmail.com>
2015-08-06 15:19:01 +0800

    06 - Detail Pop-up

--- StoreSearch/DimmingPresentationController.swift
+++ StoreSearch/DimmingPresentationController.swift
@@ -15,9 +15,21 @@ class DimmingPresentationController: UIPresentationController {
         dimmingView.frame = containerView.bounds
         
         containerView.insertSubview(dimmingView, atIndex: 0)
+        
+        dimmingView.alpha = 0.0
+        
+        if let transitionCoordinator = presentedViewController.transitionCoordinator() {
+            transitionCoordinator.animateAlongsideTransition({ _ in self.dimmingView.alpha = 1}, completion: nil)
+        }
     }
     
     override func shouldRemovePresentersView() -> Bool {
         return false
-    }    
+    }
+    
+    override func dismissalTransitionWillBegin() {
+        if let transitionCoordinator = presentedViewController.transitionCoordinator() {
+            transitionCoordinator.animateAlongsideTransition({ _ in self.dimmingView.alpha = 0}, completion: nil)
+        }
+    }
 }
---
+++ StoreSearch/SlideOutAnimationController.swift
@@ -0,0 +1,29 @@
+//
+//  SlideOutAnimationController.swift
+//  StoreSearch
+//
+//  Created by snake on 15/8/6.
+//  Copyright (c) 2015年 snakeLib. All rights reserved.
+//
+
+import UIKit
+
+class SlideOutAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
+        return 0.3
+    }
+    
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        if let fromView = transitionContext.viewForKey(UITransitionContextFromViewKey) {
+            let duration = transitionDuration(transitionContext)
+            let containerView = transitionContext.containerView()
+            
+            UIView.animateWithDuration(duration, animations: {
+                fromView.center.y -= containerView.bounds.size.height
+                fromView.transform = CGAffineTransformMakeScale(0.5, 0.5)
+                }, completion: {
+                    finished in transitionContext.completeTransition(finished)
+            })
+        }
+    }
+}
