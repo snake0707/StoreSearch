38f3a05cd3f9923ae5da759f816ae20c34ac4d0f
snake <lib0707@gmail.com>
2015-08-06 19:45:31 +0800

    Adding buttons for the search results

--- StoreSearch/LandscapeViewController.swift
+++ StoreSearch/LandscapeViewController.swift
@@ -9,11 +9,24 @@
 import UIKit
 
 class LandscapeViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.removeConstraints(view.constraints())
+        view.setTranslatesAutoresizingMaskIntoConstraints(true)
+        
+        scrollView.removeConstraints(scrollView.constraints())
+        scrollView.setTranslatesAutoresizingMaskIntoConstraints(true)
+        
+        pageControl.removeConstraints(pageControl.constraints())
+        pageControl.setTranslatesAutoresizingMaskIntoConstraints(true)
+        
+        scrollView.backgroundColor = UIColor(patternImage: UIImage(named: "LandscapeBackground")!)
+        scrollView.contentSize = CGSize(width: 1000, height: 1000)
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,15 +38,12 @@ class LandscapeViewController: UIViewController {
         println("deinit \(self)")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        scrollView.frame = view.bounds
+        
+        pageControl.frame = CGRect(x: 0, y: view.frame.size.height - pageControl.frame.size.height, width: view.frame.size.width, height: pageControl.frame.size.height)
     }
-    */
 
 }
