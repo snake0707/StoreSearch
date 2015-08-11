d391598f812f42a9dcb1b9a05b43d7b94d8ac66a
snake <lib0707@gmail.com>
2015-08-11 11:55:05 +0800

    Improving the detail pane

--- StoreSearch/AppDelegate.swift
+++ StoreSearch/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         customizeAppearance()
+        detailViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
         return true
     }
 
@@ -47,7 +48,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window!.tintColor = UIColor(red: 10/255, green: 80/255, blue: 80/255, alpha: 1)
     }
-
-
+    
+    var splitViewController: UISplitViewController {
+        return window!.rootViewController as! UISplitViewController
+    }
+    
+    var searchViewController: SearchViewController {
+        return splitViewController.viewControllers.first as! SearchViewController
+    }
+    
+    var detailNavigationController: UINavigationController {
+        return splitViewController.viewControllers.last as! UINavigationController
+    }
+    
+    var detailViewController: DetailViewController {
+        return detailNavigationController.topViewController as! DetailViewController
+    }
 }
 
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -39,6 +39,8 @@ class SearchViewController: UIViewController {
         tableView.registerNib(cellNib, forCellReuseIdentifier: TableViewCellIdentifiers.loadingCell)
         
         searchBar.becomeFirstResponder()
+        
+        title = NSLocalizedString("Search", comment: "Split-view master button")
     }
     
     override func willTransitionToTraitCollection(newCollection: UITraitCollection, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
